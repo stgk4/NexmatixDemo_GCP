dce7531c17e789670630a347cc25501e5301840f
Srinivas chakravarthi thandu <thandus@ociweb.com>
2017-08-08 01:11:18 -0500

    Working code for valve status: transaction model implementation

--- CloudFunctions/state/index.js
+++ CloudFunctions/state/index.js
@@ -6,7 +6,6 @@
 *
 */
 
-
 // Specifying datastore requirement in GCP project
 const Datastore = require('@google-cloud/datastore');
 
@@ -54,8 +53,8 @@ exports.subscribe = function subscribe (event, callback) {
 // [END functions_pubsub_subscribe]
 
 function createEntity(jsonData){
-console.log("Entered createEntity");
-    var manifold_key = jsonData.manifold_sn;
+console.log("Entered createEntity...");
+    var manifold_sn = jsonData.manifold_sn;
     const transaction = datastore.transaction();
       for(var i = 0; i < jsonData.stations.length; i++){
             console.log("Iteration: "+i);
@@ -64,11 +63,11 @@ console.log("Entered createEntity");
             const station_num = station.station_num;
             const valve_sn = station.valve_sn;
 
-            var cclExceeded = (station.cc > station.ccl)? true:false;
+            var isCCLExceeded = (station.cc > station.ccl)? true:false;
             var isPressureFault_notReported = (station.p_fault == 'N')?true:false;
             var isLeak_notReported = (station.leak=='N')?true:false;
 
-            var valveStatusKey = manifold_key + "." + station_num + "." + valve_sn;
+            var valveStatusKey = manifold_sn + "." + station_num + "." + valve_sn;
             var valveAlertKey_pFault = valve_sn + "." + TYPE_PRESSURE_FAULT;
             var valveAlertKey_leak = valve_sn + "." + TYPE_LEAK;
             var valveAlertKey_CCL = valve_sn + "." + TYPE_C_THRESH_FAULT;
@@ -86,53 +85,79 @@ console.log("Entered createEntity");
                     // If valve status entity exists
                     if(retrieved_entity){
                         //update
-                        console.log("Updated a valve Status Entity with key:"+retrieved_entity);
-                        var entity = createValveStatusEntity(station, manifold_key, retrieved_key_status);
+                        console.log("Updated a valve Status Entity with key:"+retrieved_key_status.name);
+                        var entity = createValveStatusEntity(station, manifold_sn, retrieved_key_status);
                         return transaction.save(entity);
                     }else{
                         // Insert valve status
-                        console.log("VStatusKey_retrieved:"+valveStatusKey);
+                        console.log("VStatusKey_retrieved:"+retrieved_key_status.name);
                         //console.log("Created a valve Status Entity with key:"+retrieved_key.name);
-                        var entity = createValveStatusEntity(station, manifold_key, retrieved_key_status);
+                        var entity = createValveStatusEntity(station, manifold_sn, retrieved_key_status);
                         return transaction.save(entity);
                     }
                 })
 
                 // fetch valveAlert for pressure fault
-               /* .then (()=>datastore.key([KIND_VALVE_ALERT,valveAlert_pFault]))
+              /*  .then (()=>transaction.get(retrieved_key_alert_p_fault))
                 .then((results)=> {
-                    const retrieved_key = results[0];
+                    const retrieved_entity = results[0];
                     //case1: if valve_sn.alert_type exists && p_fault =='N'
-                    if(retrieved_key && isPressureFault_notReported){
-                        return transaction.delete(retrieved_key);
+                    if(retrieved_entity && isPressureFault_notReported){
+                        return transaction.delete(retrieved_key_alert_p_fault);
                     }
                     //case2: if valve_sn.alert_type does not exist && p_fault = 'H' or 'L'
-                    else if(!retrieved_key && !isPressureFault_notReported){
-                        return transaction.save(createValveStatusEntity(station,TYPE_PRESSURE_FAULT));
+                    else if(!retrieved_entity && !isPressureFault_notReported){
+
+                        message_time = new Date().getTime();
+                        var entity = createValveAlertEntity(station, TYPE_PRESSURE_FAULT, retrieved_key_alert_p_fault, message_time);
+                        return transaction.save(entity);
                     }
                     //Case3: if valve_sn.alert_type exists && p_fault = 'H' or 'L'
-                    else if(retrieved_key && !isPressureFault_notReported){
-                        //update function
+                    else if(retrieved_entity && !isPressureFault_notReported){
 
+                        //update function
+                        message_time = retrieved_entity.update_time;
+                        var entity = createValveAlertEntity(station, TYPE_PRESSURE_FAULT, retrieved_key_alert_p_fault, message_time);
+                        return transaction.save(entity);
                     }
                     //Case4: if valve_sn.alert_type does not exist && p_fault == 'N'
                     else{
                         //Do Nothing
+                    }
+                })
+
+                 // fetch valveAlert for cc_Threshold
+                .then (()=>transaction.get(retrieved_key_alert_c_thresh))
+                .then((results)=> {
+                    const retrieved_entity = results[0];
+                    //case1: if cc>ccl && valve_sn.alert_type does not exists
+                    if(isCCLExceeded && !retrieved_entity){
+                       message_time = new Date().getTime();
+                       var entity = createValveAlertEntity(station, TYPE_C_THRESH_FAULT, retrieved_key_alert_c_thresh, message_time);
+                       return transaction.save(entity);
+                    }
+                    //case2: if cc>ccl && valve_sn.alert_type exists
+                    else if(isCCLExceeded && retrieved_entity){
+                        message_time = retrieved_entity.update_time;
+                        var entity = createValveAlertEntity(station, TYPE_C_THRESH_FAULT, retrieved_key_alert_c_thresh, message_time);
+                        return transaction.save(entity);
+                    }
+                    //Case3: if cc<=ccl
+                    else{
+                         //Do Nothing
                     }
                 })*/
 
                 .then(()=> {
                    transaction.commit();
                    console.log("Transaction Committed");
-                   })
+                })
                 .catch((exception)=> {
-                transaction.rollback();
-                console.log("Transaction Rolledback:"+exception);
+                    transaction.rollback();
+                    console.log("Transaction Rolledback:"+exception);
                 });
-      }
-}
-
-
+            }
+    }
 
 function createValveStatusEntity(jsonData, manifold_sn, key){
     message_time = new Date().getTime();
@@ -175,23 +200,7 @@ function createValveStatusEntity(jsonData, manifold_sn, key){
     return entity;
 }
 
-/*name: valve_sn : jsonData.valve_sn,
-                  manifold_sn: jsonData.manifold_sn,
-                  station_num:jsonData.station_num,
-                  update_time: new Date().getTime(),
-                  input: jsonData.input,
-                  cc: jsonData.cc,
-                  pp:jsonData.pp,
-                  ccl:jsonData.ccl,
-                  p_fault: jsonData.p_fault,
-                  leak: jsonData.leak*/
-
-
-
-
-
-
-function createValveAlertEntity(jsonData, key, alert_type){
+function createValveAlertEntity(jsonData, alert_type, key, message_time){
     console.log("Creating ValveAlertEntity...")
     if(alert_type==TYPE_PRESSURE_FAULT){
         description = ((jsonData.p_fault == 'H') ? "High":"Low") + " pressure fault detected";
@@ -211,7 +220,7 @@ function createValveAlertEntity(jsonData, key, alert_type){
             },
             {
                 name: 'detection_time',
-                value: new Date().getTime() //do a query for old time stamp
+                value: message_time //do a query for old time stamp
             },
             {
                 name: 'alert_type',
@@ -250,76 +259,3 @@ function updateEntity (transaction, valve_sn, alert_type, description) {
     );
 }
 // [END update_entity]
-
-// [START delete_entity]
-function deleteAlert (transaction, valve_sn, alert_type) {
-    const retrieved_key_del = transaction.key(
-        [
-            KIND_VALVE_ALERT,
-            valve_sn + '.' + alert_type
-        ]
-    );
-    transaction.delete(retrieved_key_del)
-}
-// [END delete_entity]
-
-// [START addEntity]
-function addEntity (transaction, entity) {
-    transaction.save(entity);
-}
-// [END add_entity]
-
-/**
- * Gets a Datastore key from the kind/key pair in the request.
- *
- * @param {object} requestData Cloud Function request data.
- * @param {string} requestData.key Datastore key string.
- * @param {string} requestData.kind Datastore kind.
- * @returns {object} Datastore key object.
- */
-function getKeyFromRequestData (requestData) {
-  if (!requestData.key) {
-    throw new Error('Key not provided. Make sure you have a "key" property in your request');
-  }
-
-  if (!requestData.kind) {
-    throw new Error('Kind not provided. Make sure you have a "kind" property in your request');
-  }
-
-  return transaction.key([requestData.kind, requestData.key]);
-}
-
-/**
- * Creates and/or updates a record.
- *
- * @example
- * gcloud alpha functions call set --data '{"kind":"Task","key":"sampletask1","value":{"description": "Buy milk"}}'
- *
- * @param {object} req Cloud Function request context.
- * @param {object} req.body The request body.
- * @param {string} req.body.kind The Datastore kind of the data to save, e.g. "Task".
- * @param {string} req.body.key Key at which to save the data, e.g. "sampletask1".
- * @param {object} req.body.value Value to save to Cloud Datastore, e.g. {"description":"Buy milk"}
- * @param {object} res Cloud Function response context.
- */
-exports.set = function set (req, res) {
-    // The value contains a JSON document representing the entity we want to save
-    if (!req.body.value) {
-        throw new Error('Value not provided. Make sure you have a "value" property in your request');
-    }
-
-    const key = getKeyFromRequestData(req.body);
-    const entity = {
-        key: key,
-        data: req.body.value
-    };
-
-    return datastore.save(entity)
-        .then(() => res.status(200).send(`Entity ${key.path.join('/')} saved.`))
-        .catch((err) => {
-            console.error(err);
-            res.status(500).send(err);
-            return Promise.reject(err);
-        }
-    );
-};
